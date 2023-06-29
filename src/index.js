2b30f1f20d33617083fcc49c6097d0cc297862b5
mcarroll138 <mcarroll138@gmail.com>
2023-06-29 15:48:05 -0700

    adjust HTML and CSS changes.
    Co-Authorized-By: Michael Carroll mcarrol138@gmail.com

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import '../css/styles.css';
 import ApiRequest from './ApiRequest.js'
 
-
 const button = document.querySelector('#button');
 
 button.addEventListener('click', function () {
@@ -9,43 +8,52 @@ button.addEventListener('click', function () {
   ul.innerText = '';
   ApiRequest.send()
     .then(response => {
+      //Show JASON file (response)
       console.log(response);
 
+      //Get variabels
       const img = document.querySelector('img');
+      const instructionsP = document.querySelector('#instructions');
+      let displayDiv = document.querySelector('#display-results');
+      const cocktailName = document.querySelector('#cocktail-name');
       let arrayIng = [];
       let arrayMeasure = [];
+      let li = [];
 
-      let displayDiv = document.querySelector('#display-results');
+      //Turn visible
       displayDiv.classList.remove('hidden');
 
-      document.querySelector('#cocktail-name').innerHTML = `${response.drinks[0].strDrink}`;
-
+      //Set varbs new content
+      cocktailName.innerHTML = `${response.drinks[0].strDrink}`;
       img.src = `${response.drinks[0].strDrinkThumb}`;
+      instructionsP.innerHTML = `${response.drinks[0].strInstructions}`;
 
+      //Empty 'ul' container
       ul.innerHTML = '';
 
+      //Run logic to chack validition
       for (let i = 0; i <= 15; i++) {
-
         if ((response.drinks[0][`strIngredient${i + 1}`] === null || response.drinks[0][`strIngredient${i + 1}`] === ' ') && (response.drinks[0][`strMeasure${i + 1}`] === null || response.drinks[0][`strMeasure${i + 1}`] === ' ')) {
           return;
-
         } else if (response.drinks[0][`strMeasure${i + 1}`] === null || response.drinks[0][`strMeasure${i + 1}`] === ' ') {
           arrayMeasure[i] = ' ';
-
         } else if (response.drinks[0][`strIngredient${i + 1}`] === null || response.drinks[0][`strIngredient${i + 1}`] === ' ') {
           arrayIng[i] = ' ';
-
         } else {
+          //Push name of cocktail to an array
           arrayIng.push(response.drinks[0][`strIngredient${i + 1}`]);
+          //Push measurements to an array
           arrayMeasure.push(response.drinks[0][`strMeasure${i + 1}`]);
           //create li
-          let li = [];
           li[i] = document.createElement('li');
-          //set the value of the li
+          //set the value of the new li to measurement + name
           li[i].innerText = arrayMeasure[i] + ' ' + arrayIng[i];
           //append li to ul
           ul.appendChild(li[i])
         }
+        //Until here loop
       }
+
+      //Until here ApiResquest
     });
 });
